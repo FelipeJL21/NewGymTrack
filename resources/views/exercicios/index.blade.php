721c4c347b17ea4e1ebbd78731eb1b3cbc454d52
FelipeJL21 <felipejazonlima@gmail.com>
2025-11-13 21:47:07 -0300

    corrigindo Erro

---
+++ resources/views/exercicios/index.blade.php
@@ -0,0 +1,13 @@
+<div>
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   </head>
+   <body>
+    
+   </body>
+   </html> <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+</div>
